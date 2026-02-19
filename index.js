b6b1052af812a19506437ee0d74ee286baa702a7
ducky <asqrybot@gmail.com>
2026-02-19 18:53:42 -0500

    feat: towers timer, scene change

--- index.js
+++ index.js
@@ -17,16 +17,51 @@ async function connect() {
     }
 }
 
+function createPacket(command, data) {
+	return JSON.stringify({ command, data, id: 0 });
+}
+
+
+const towerSocket = new WebSocket(process.env.TOWERS_WS_URI)
+
+towerSocket.onmessage = (ev) => {
+    let packet = JSON.parse(ev.data);
+
+    switch(packet.command) {
+        case "hello": {
+            console.log(`Towers Socket Connected`)
+            towerSocket.send(createPacket("hello", {intents: ['ROSTER']}))
+            break;
+        }
+        case "timer": {
+            console.log("TIMER", packet)
+            break;
+        }
+
+        case "nope": {
+            console.log("Towers Socket denied permission", packet)
+            break;
+        }
+    }
+
+}
+
 const obs = new OBSWebSocket();
 const webserver = express();
 
 webserver.use(express.json());
 
+let scenes = {
+    'Preview': "none",
+    'Program': "none"
+};
 let transition = "none";
 let connected = false;
 let failures = 0;
 let interval;
 let error = "none";
+
+
 
 (async () => {
     await connect();
@@ -100,6 +135,55 @@ webserver.get("/set-transition/:transition", async (req, res) => {
 
         }
     })
+})
+
+webserver.get("/set-scene/:scene/:type", async (req, res) => {
+    if(!connected) await connect();
+    let fail = false;
+    if(!connected) {
+        if(error === "none") {res.sendStatus(503);} else {
+            res.statusCode = 503;
+            res.send({error})
+        }
+        console.log("Restart OBS or Service. Error: ", error)
+        return;
+    }
+    if(scenes[req.params.type] === req.params.scene) {
+        res.sendStatus(200);
+        console.log(`Skipped setting scene ${scenes[req.params.type]} -> ${req.params.scene}`)
+        return;
+    }
+
+    let validTypes = ["Program", "Preview"];
+
+    if(!req.params.type || !validTypes.includes(req.params.type)) {
+        res.statusCode = 401;
+        res.send({error: `Argument "type" must be one of the following: ${validTypes.map(t => `"${t}"`).join(", ")} You provided "${req.params.type || "Nothing"}"`})
+        return;
+    }
+    await obs.call(`SetCurrent${req.params.type}Scene`, {sceneName: req.params.scene}).catch(e => {
+        res.statusCode = 401;
+        res.send({error: e.message})
+        console.log("failed to set scene to " + req.params.scene, e)
+        fail = true;
+    }).finally(() => {
+        if(!fail) res.sendStatus(200)
+        if(!fail) {
+            console.log(`Setting scene ${scenes[req.params.type]} -> ${req.params.scene} on ${req.params.type.toUpperCase()}`)
+            scenes[req.params.type] = req.params.scene;
+
+        }
+    })
+})
+
+webserver.get("/timer/:minutes", async (req, res) => {
+    try {
+        towerSocket.send(createPacket("duckTimer", (req.params.minutes)))
+        res.sendStatus(200);
+    } catch(e) {
+        res.statusCode = 401;
+        res.send({error: e})
+    }
 })
 
 webserver.listen(process.env.HTTP_PORT, (e) => {
